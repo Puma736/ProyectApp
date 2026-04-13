feadf384f7c922f416b6b847768af23b916a2e17
FabioPuma <7362pumamebchaca@gmail.com>
2026-04-12 22:26:21 -0400

    actualizando configuracion y rutas

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
 export default function Home() {
   return (
-    <main style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      alignItems: 'center', 
-      justifyContent: 'center', 
+    <main style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
       height: '100vh',
       fontFamily: 'sans-serif',
       backgroundColor: '#ffffff'
     }}>
-      <h1 style={{ color: '#000000', marginBottom: '10px' }}>Mi Aplicación</h1>
-      <p style={{ color: '#666', marginBottom: '30px' }}>Haz clic abajo para iniciar la descarga segura.</p>
-      
-    
-      <a 
-        href="https://google.com" 
-        target="_blank" 
+      <h1 style={{ color: '#000000', marginBottom: '10px' }}>
+        Mi Aplicación
+      </h1>
+
+      <p style={{ color: '#666', marginBottom: '30px' }}>
+        Haz clic abajo para iniciar la descarga segura.
+      </p>
+
+      <a
+        href="https://google.com"
+        target="_blank"
         rel="noopener noreferrer"
         style={{
           padding: '16px 32px',
